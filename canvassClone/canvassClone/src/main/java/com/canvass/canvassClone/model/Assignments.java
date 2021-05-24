5926c8761bec1d1800d61be8cf6fc80d566f7057
MayankBatra005 <er.mayank005@gmail.com>
2021-05-24 07:11:45 -0700

    To String boiler plate code removed.

--- canvassClone/canvassClone/src/main/java/com/canvass/canvassClone/model/Assignments.java
+++ canvassClone/canvassClone/src/main/java/com/canvass/canvassClone/model/Assignments.java
@@ -1,14 +1,12 @@
 package com.canvass.canvassClone.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 public class Assignments {
     public int id;
     public String Name;
@@ -17,15 +15,5 @@ public class Assignments {
     public String question2;
     public String answer2;
 
-    @Override
-    public String toString() {
-        return "Assignments{" +
-                "id=" + id +
-                ", Name='" + Name + '\'' +
-                ", question1='" + question1 + '\'' +
-                ", answer1='" + answer1 + '\'' +
-                ", question2='" + question2 + '\'' +
-                ", answer2='" + answer2 + '\'' +
-                '}';
-    }
+
 }
